dd6ca72f4c5543f37a8fdff679e4e952544e0ea9
V1GENIUS <Vivekrathore9111@gmail.com>
2025-01-07 22:46:49 +0530

    finels commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Use CORS middleware
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://sarkarigenius.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'], 
 }));
 
