344ccc2d187ef7c6e5ac5c2b98ef797a46354bd7
V1GENIUS <Vivekrathore9111@gmail.com>
2025-01-08 11:35:34 +0530

    finel

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const jobRoutes = require('./routers/jobRoutes');
 const visitorRoutes = require('./routers/visitorsRoute');
 const connectDB = require('./Database/connectDB');
 const userRoutes = require("./routers/userRoutes");
-
+require('dotenv').config();
 const app = express();
 
 // Use CORS middleware
 app.use(cors({
-  origin: 'https://sarkarigenius.vercel.app', 
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'], 
 }));
 
 
 
 app.use(express.json());
-const port = process.env.PORT || 7000;
+
 connectDB();
 
 
@@ -28,8 +28,11 @@ app.use("/api/jobs", jobRoutes);
 app.use("/api/user", userRoutes);
 
 
+const port = process.env.PORT || 7000 ;
 
-
+if (!port) {
+  throw new Error("PORT is not defined in the environment variables.");
+}
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
