6e593aa58e09a8cba251b743c7e2dc27e734a8d2
Ritika J <vivekrathore9111@gmail.com>
2025-04-23 14:57:53 +0530

    forget add

--- Database/connectDB.js
+++ Database/connectDB.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 MONGO = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://SarkariGenius:Ektuhinirankar123@geniuscluster.wsjvs.mongodb.net/?retryWrites=true&w=majority'|| process.env.MONGO);
+    const conn = await mongoose.connect('mongodb+srv://SarkariGenius:Ektuhinirankar123@geniuscluster.wsjvs.mongodb.net/?retryWrites=true&w=majority&appName=GeniusCluster');
+    // mongodb+srv://SarkariGenius:Ektuhinirankar123@geniuscluster.wsjvs.mongodb.net/?retryWrites=true&w=majority&appName=GeniusCluster
     console.log("Successfully connected to MongoDB!");
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
